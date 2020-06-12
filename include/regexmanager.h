2ec66a17b2eb3bfaec1685808bf1e395c9b5b460
Dennis van der Schagt <dennisschagt@gmail.com>
2020-06-12 19:50:17 +0200

    Use StflString in RegexManager::quote_and_highlight()

--- include/regexmanager.h
+++ include/regexmanager.h
@@ -24,11 +24,6 @@ public:
 	void quote_and_highlight(std::string& str, const std::string& location);
 	void remove_last_regex(const std::string& location);
 	int article_matches(Matchable* item);
-	std::map<size_t, std::string> extract_style_tags(std::string& str);
-	void insert_style_tags(std::string& str, std::map<size_t, std::string>& tags);
-	void merge_style_tag(std::map<size_t, std::string>& tags,
-		const std::string& tag,
-		size_t start, size_t end);
 	std::string get_attrs_stfl_string(const std::string& location, bool hasFocus);
 
 private:
