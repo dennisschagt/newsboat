b41a0f44388f1a32ac01523bb8962e06bb9f3692
Dennis van der Schagt <dennisschagt@gmail.com>
2024-07-15 21:22:25 +0200

    Return Filepath from TempFile

--- test/cache.cpp
+++ test/cache.cpp
@@ -1,5 +1,3 @@
-#define ENABLE_IMPLICIT_FILEPATH_CONVERSIONS
-
 #include "cache.h"
 
 #include <memory>
@@ -982,7 +980,7 @@ TEST_CASE(
 	// can't be done by direct assignment because sqlite3_open doesn't
 	// return that pointer)
 	sqlite3* dbptr = nullptr;
-	int error = sqlite3_open(dbfile.get_path().c_str(), &dbptr);
+	int error = sqlite3_open(dbfile.get_path().to_locale_string().c_str(), &dbptr);
 	REQUIRE(error == SQLITE_OK);
 	db.reset(dbptr);
 	dbptr = nullptr;
